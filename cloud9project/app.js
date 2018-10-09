813a2170fc04bcd21bc2384a4a047feca9fc6ede
poukkyine-mdk <pyaephyokyaw.22121996@outlook.com>
2018-10-09 03:25:30 +0000

    save

--- cloud9project/app.js
+++ cloud9project/app.js
@@ -3,10 +3,10 @@
 const cassandra = require('cassandra-driver');
 var createKsQuery = "CREATE KEYSPACE kmd_blackboard WITH REPLICATION = {  'class' : 'NetworkTopologyStrategy',  'datacenter1' : 1 } ;";
 const client = new cassandra.Client({
-  contactPoints: ['172.18.0.22']
+  contactPoints: ['172.18.0.2']
 });
 client.connect()
-  .catch(function (err) {
+  .catch(function(err) {
     console.error('There was an error when connecting', err);
     return client.shutdown();
   });
@@ -28,66 +28,72 @@ app.use(session({
 app.use(express.static(__dirname + "/public"));
 /* *************************************************** */
 /* Landing */
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("landing");
 });
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.locals.currentUser = req.session.username;
   next();
 });
 /* *************************************************** */
 /* login */
-app.post("/login", function (req, res) {
+app.post("/login", function(req, res) {
 
   if (req.body.username != "") {
     client
       .execute("SELECT * FROM kmd_blackboard.users WHERE username='" + req.body.username + "'",
-        function (err, result) {
+        function(err, result) {
           if (!err) {
             if (result.rowLength == 1) {
 
               if (result.rows[0].username == req.body.username && result.rows[0].password == req.body.password) {
                 req.session.username = result.rows[0].username;
                 res.redirect("/courses");
-              } else {
+              }
+              else {
                 req.session.ok = false;
                 res.redirect("/login")
               }
-            } else {
+            }
+            else {
               req.session.ok = false;
               res.redirect("/login")
             }
 
-          } else {
+          }
+          else {
             console.log(err);
           }
         })
-  } else {
+  }
+  else {
     req.session.ok = false;
     res.redirect("/login")
   }
 })
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res) {
 
   if (!req.session.username) {
 
     var ok;
     if (req.session.ok == false) {
       ok = req.session.ok;
-    } else {
+    }
+    else {
       ok = true;
     }
     res.render("login", {
       ok: ok
     });
-  } else {
+  }
+  else {
 
     res.redirect("/courses")
   }
 })
 /* *************************************************** */
 /**  Log Out **/
-app.get("/logout", function (req, res) {
+app.get("/logout", function(req, res) {
   if (req.session.username) {
     req.session.destroy();
     req.session = null;
@@ -97,35 +103,104 @@ app.get("/logout", function (req, res) {
 
 /* *************************************************** */
 /**  Show all courses **/
-app.get("/courses", function (req, res) {
+app.get("/courses", function(req, res) {
   if (req.session.username) {
     var username = req.session.username;
 
     client.execute("SELECT * FROM kmd_blackboard.courses_by_user WHERE username='" + username + "'",
-      function (err, result) {
+      function(err, result) {
         if (!err) {
           var rows = result.rows;
           res.render("courses", {
             rows,
             rows
           })
-        } else {
+        }
+        else {
           console.log(err);
         }
       })
-  } else {
+  }
+  else {
     res.redirect("/login");
   }
 });
 /* *************************************************** */
 /**  Show one course **/
-app.get("/courses/:id", function (req, res) {
+app.get("/courses/:id", function(req, res) {
   if (req.session.username) {
-  var coursecode = req.params.id;
-  client.execute("SELECT * FROM kmd_blackboard.courses_by_code WHERE coursecode='" + coursecode + "'",
+    var coursecode = req.params.id;
+    client.execute("SELECT * FROM kmd_blackboard.courses_by_code WHERE coursecode='" + coursecode + "'",
+      function(err, result) {
+        if (!err) {
+          res.render("coursedetails", {
+            result: result
+          })
+        }
+        else {
+          console.log(err);
+        }
+      })
+  }
+  else {
+    res.redirect("/login");
+  }
+});
+
+/* View lectures */
+app.get("/courses/:id/lectures", function(req, res) {
+  if (req.session.username) {
+    var coursecode = req.params.id;
+    client.execute("SELECT * FROM kmd_blackboard.lectures_by_code WHERE coursecode='" + coursecode + "'",
+      function(err, result) {
+        if (!err) {
+          res.render("lectures", {
+            result: result
+          })
+        }
+        else {
+          console.log(err);
+        }
+      })
+  }
+  else {
+    res.redirect("/login");
+  }
+})
+
+/* View Grades */
+app.get("/courses/:id/grades", function(req, res) {
+  if (req.session.username) {
+    var username = req.session.username;
+    var coursecode = req.params.id;
+    client.execute("SELECT * FROM kmd_blackboard.grades_by_username_code WHERE username='" + username + "' AND coursecode='" + coursecode + "'",
+      function(err, result) {
+        if (!err) {
+          console.log(result)
+          res.render("grades", {
+            result: result
+          })
+        }
+        else {
+          console.log(err);
+        }
+      })
+  }
+  else {
+    res.redirect("/login");
+  }
+})
+
+/* View Assessment Information */
+app.get("/courses/:id/assessments", function(req, res) {
+    if (req.session.username) {
+      var username=req.session.username;
+    var coursecode= req.params.id;
+    client.execute("SELECT * FROM kmd_blackboard.grades_by_username_code WHERE username='"+username+"' AND coursecode='" + coursecode + "'",
     function (err, result) {
       if (!err) {
-        res.render("coursedetails", {
+        console.log(result)
+        res.render("assessment", {
           result:result
         })
       } else {
@@ -135,84 +210,29 @@ app.get("/courses/:id", function (req, res) {
   }else{
     res.redirect("/login");
   }
-});
 
-/* View lectures */
-app.get("/courses/:id/lectures",function(req,res){
-  if (req.session.username) {
-  var coursecode= req.params.id;
-  client.execute("SELECT * FROM kmd_blackboard.lectures_by_code WHERE coursecode='" + coursecode + "'",
-  function (err, result) {
-    if (!err) {
-      res.render("lectures", {
-        result:result
-      })
-    } else {
-      console.log(err);
-    }
-  })
-}else{
-  res.redirect("/login");
-}
 })
 
-/* View Grades */
-app.get("/courses/:id/grades",function(req,res){
-  if (req.session.username) {
-    var username=req.session.username;
-  var coursecode= req.params.id;
-  client.execute("SELECT * FROM kmd_blackboard.grades_by_username_code WHERE username='"+username+"' AND coursecode='" + coursecode + "'",
-  function (err, result) {
-    if (!err) {
-      console.log(result)
-      res.render("grades", {
-        result:result
-      })
-   } else {
-     console.log(err);
-   }
- })
-}else{
- res.redirect("/login");
-}
-})
 
-/* View Assessment Information */
-app.get("/courses/:id/assessments",function(req,res){
-//   if (req.session.username) {
-//     var username=req.session.username;
-//   var coursecode= req.params.id;
-//   client.execute("SELECT * FROM kmd_blackboard.grades_by_username_code WHERE username='"+username+"' AND coursecode='" + coursecode + "'",
-//   function (err, result) {
-//     if (!err) {
-//       console.log(result)
-//       res.render("grades", {
-//         result:result
-//       })
-//    } else {
-//      console.log(err);
-//    }
-//  })
-// }else{
-//  res.redirect("/login");
-// }
-res.render("assessment")
-})
+
+
+
+
 // CREATE - Adds new course to db
-app.post("/courses", function (req, res) {
+app.post("/courses", function(req, res) {
   res.redirect("/courses")
 });
 
 // NEW - Shows form to add new courses
-app.get("/courses/new", function (req, res) {
+app.get("/courses/new", function(req, res) {
   res.render("new.ejs");
 });
 
 // SHOW - Shows info about courses
-app.get("/courses/:id", function (req, res) {
+app.get("/courses/:id", function(req, res) {
   res.send("hi")
 });
 
-app.listen(80, process.env.IP, function () {
+app.listen(8080, process.env.IP, function() {
   console.log("Server has Started!!!");
 });
--- cloud9project/seed.js
+++ cloud9project/seed.js
@@ -1,52 +1,65 @@
 "use strict";
 const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({
-    contactPoints: ['172.18.0.22']
+    contactPoints: ['172.18.0.2']
 });
 
 client.connect()
-    //  .then(()=> client.execute("CREATE KEYSPACE kmd_blackboard WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy',  'datacenter1' : 1 } "))
+    //  .then(()=> client.execute("CREATE KEYSPACE kmd_blackboard WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy',  'datacenter1' : 2 } "))
     //  .then(() => client.execute("USE kmd_blackboard"))
     //  .then(() => client.execute("CREATE TABLE kmd_blackboard.users( username text PRIMARY KEY, password text)"))
     //  .then(() => client.execute("CREATE TABLE kmd_blackboard.courses_by_user(username text, courseCode text, courseName text, courseLecturer text, PRIMARY KEY(username,courseCode))"))
     //  .then(() => client.execute("CREATE TABLE kmd_blackboard.courses_by_code (coursecode text, coursename text, courselecturer text, PRIMARY KEY(coursecode))"))
     //  .then(() => client.execute("CREATE TABLE kmd_blackboard.lectures_by_code (coursecode text, week text, slides set<text>, recordings set<text>, PRIMARY KEY(coursecode,week))"))
     //  .then(() => client.execute("CREATE TABLE kmd_blackboard.grades_by_username_code (username text, coursecode text, grades map<text,text>, PRIMARY KEY(username,coursecode))"))
-   
+    //  .then(() => client.execute("CREATE TABLE kmd_blackboard.assessment_info_by_coursecode (coursecode text, posts map<text,text>, PRIMARY KEY(coursecode))"))
 
-       .then(() => client.execute("CREATE TABLE kmd_blackboard.assessment_info_by_coursecode (coursecode text, posts map<text,text>, PRIMARY KEY(coursecode))"))
- 
-    //  .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS2341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
-    //  .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS2341','2',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC81','https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E','https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC82'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
-    //  .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS3241','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
-    //  .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS5341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
-    //  .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS6341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
-    //  .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS7341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
-    //  .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS8341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
+//      .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS2341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
+//      .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS2341','2',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC81','https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E','https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC82'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
+//      .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS3241','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
+//      .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS5341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
+//      .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS6341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
+//      .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS7341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
+//      .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS8341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
 
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.users(username,password) VALUES ('pk','123')" ))
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS2341','Web Dev','Mr.T')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS3241','CS Principles','Mr.G')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS5341','A+','Mr.V')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS6341','Programming in Large','Mr.B')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS7341','Information Retrieval','Mr.C')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS8341','Dream League','Mr.A')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.users(username,password) VALUES ('pk','123')" ))
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS2341','Web Dev','Mr.T')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS3241','CS Principles','Mr.G')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS5341','A+','Mr.V')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS6341','Programming in Large','Mr.B')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS7341','Information Retrieval','Mr.C')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS8341','Dream League','Mr.A')") )
 
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS2341','Web Dev','Mr.T')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS3241','CS Principles','Mr.G')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS5341','A+','Mr.V')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS6341','Programming in Large','Mr.B')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS7341','Information Retrieval','Mr.C')") )
-    // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS8341','Dream League','Mr.A')") )
-    
-       .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS2341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
-    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS3241',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
-    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS5341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
-    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS6341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
-    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS7341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
-    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS8341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
-    // .then(() => client.execute("DROP KEYSPACE kmd_blackboard") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS2341','Web Dev','Mr.T')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS3241','CS Principles','Mr.G')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS5341','A+','Mr.V')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS6341','Programming in Large','Mr.B')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS7341','Information Retrieval','Mr.C')") )
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS8341','Dream League','Mr.A')") )
+
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS2341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS3241',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS5341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS6341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS7341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+//     .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS8341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+
+     .then(() => client.execute("INSERT INTO kmd_blackboard.assessment_info_by_coursecode(coursecode, posts) VALUES ('INFS2341',{'10/9/2018':'Dear students, the purpose of asssignment 2 is for creating cloud computing project without using any code.','11/9/2018':'There will be mutiple choices.','12/92018':'Final Exam'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.assessment_info_by_coursecode(coursecode, posts) VALUES ('INFS3241',{'10/9/2018':'Dear students, the purpose of asssignment 2 is for creating cloud computing project without using any code.','11/9/2018':'There will be mutiple choices.','12/92018':'Final Exam'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.assessment_info_by_coursecode(coursecode, posts) VALUES ('INFS5341',{'10/9/2018':'Dear students, the purpose of asssignment 2 is for creating cloud computing project without using any code.','11/9/2018':'There will be mutiple choices.','12/92018':'Final Exam'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.assessment_info_by_coursecode(coursecode, posts) VALUES ('INFS6341',{'10/9/2018':'Dear students, the purpose of asssignment 2 is for creating cloud computing project without using any code.','11/9/2018':'There will be mutiple choices.','12/92018':'Final Exam'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.assessment_info_by_coursecode(coursecode, posts) VALUES ('INFS7341',{'10/9/2018':'Dear students, the purpose of asssignment 2 is for creating cloud computing project without using any code.','11/9/2018':'There will be mutiple choices.','12/92018':'Final Exam'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.assessment_info_by_coursecode(coursecode, posts) VALUES ('INFS8341',{'10/9/2018':'Dear students, the purpose of asssignment 2 is for creating cloud computing project without using any code.','11/9/2018':'There will be mutiple choices.','12/92018':'Final Exam'})" ))
+
+
+
+
+
+
+
+
+//     .then(() => client.execute("DROP KEYSPACE kmd_blackboard") )
     .then(() => client.shutdown())
-    .catch(function (err) {
-       console.log(err);
+    .catch(function(err) {
+        console.log(err);
     });
