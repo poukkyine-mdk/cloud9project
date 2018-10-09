e588fd0721d29c65905e2ea648100712b44a4b72
EC2 Default User <ec2-user@ip-172-31-8-176.us-west-2.compute.internal>
2018-10-09 03:04:18 +0000

    seed update

--- cloud9project/seed.js
+++ cloud9project/seed.js
@@ -12,9 +12,9 @@ client.connect()
     //  .then(() => client.execute("CREATE TABLE kmd_blackboard.courses_by_code (coursecode text, coursename text, courselecturer text, PRIMARY KEY(coursecode))"))
     //  .then(() => client.execute("CREATE TABLE kmd_blackboard.lectures_by_code (coursecode text, week text, slides set<text>, recordings set<text>, PRIMARY KEY(coursecode,week))"))
     //  .then(() => client.execute("CREATE TABLE kmd_blackboard.grades_by_username_code (username text, coursecode text, grades map<text,text>, PRIMARY KEY(username,coursecode))"))
-    //  .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS2341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+   
 
-       .then(() => client.execute("CREATE TABLE kmd_blackboard.assessment_info_by_coursecode (username text, coursecode text, grades map<text,text>, PRIMARY KEY(username,coursecode))"))
+       .then(() => client.execute("CREATE TABLE kmd_blackboard.assessment_info_by_coursecode (coursecode text, posts map<text,text>, PRIMARY KEY(coursecode))"))
  
     //  .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS2341','1',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
     //  .then(() => client.execute("INSERT INTO kmd_blackboard.lectures_by_code(coursecode,week,slides,recordings) VALUES ('INFS2341','2',{'https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC81','https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC8E','https://drive.google.com/open?id=1iJLo_X7cF9d-Vg8O0ibRcnFBeCZdSj2zuWEeR8hmC82'},{'https://www.youtube.com/watch?v=Zftx68K-1D4'})" ))
@@ -38,6 +38,13 @@ client.connect()
     // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS6341','Programming in Large','Mr.B')") )
     // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS7341','Information Retrieval','Mr.C')") )
     // .then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_code(coursecode,coursename,courselecturer) VALUES ('INFS8341','Dream League','Mr.A')") )
+    
+       .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS2341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS3241',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS5341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS6341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS7341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
+    .then(() => client.execute("INSERT INTO kmd_blackboard.grades_by_username_code(username, coursecode, grades) VALUES ('pk','INFS8341',{'Assignment 1':'6','Mid Exam':'6','Final Exam':'7'})" ))
     // .then(() => client.execute("DROP KEYSPACE kmd_blackboard") )
     .then(() => client.shutdown())
     .catch(function (err) {
